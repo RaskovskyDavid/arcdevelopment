dd579c4b4ac4140f12b75a6f62dd4bf32d88f74a
David <davidtucu@hotmail.com>
2021-11-09 15:51:01 -0300

    style tabs and active tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { AppBar } from "@material-ui/core";
-import { Toolbar } from "@material-ui/core";
+import React, { useState } from "react";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import { makeStyles } from '@mui/styles';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Button from '@mui/material/Button';
 
 import logo from "../../assets/logo.svg";
 
@@ -25,44 +26,86 @@ function ElevationScroll(props) {
   } 
   const useStyles = makeStyles( theme => (
     {
-      root:{
-        textTransform: "none"
-      },
-      toolbarMargin:{
-        ...theme.mixins.toolbarMargin,
-        marginBottom: "3em"
-      },
-      logo:{
-        height: "7em"
-      },
-      tabContainer: {
-        marginLeft: "auto"
-      },
-      tab: {
-        ...theme.typography.tab,
-        minWidth: 10,
-        marginLeft: "25px",
-        color: "white"
-       
-      }
+      root: {      
+        toolbarMargin:{
+          ...theme.mixins.toolbarMargin,
+          marginBottom: "3em"
+        },
+        logo:{
+          height: "7em"
+        },
+        tabContainer: {
+          marginLeft: "auto",
+          indicatorColor: "white"
+        },
+        tab: {
+          ...theme.typography.tab,
+          minWidth: 10,
+          marginLeft: "25px",
+          color: "white"
+        
+        },
+        buttonstyle:{
+          ...theme.typography.estimate,
+          borderRadius: "50px",
+          marginLeft: "50px",
+          height: "45px",
+          color:"white"
+        }
+    },
+  
+    toolbarMargin:{
+      ...theme.mixins.toolbarMargin,
+      marginBottom: "3em"
+    },
+    logo:{
+      height: "7em"
+    },
+    tabContainer: {
+      marginLeft: "auto",
+      indicatorColor: "white"
+    },
+    tab: {
+      ...theme.typography.tab,
+      minWidth: 10,
+      marginLeft: "25px",
+      color: "white"
+    
+    },
+    buttonstyle:{
+      ...theme.typography.estimate,
+      borderRadius: "50px",
+      marginLeft: "50px",
+      height: "45px",
+      color:"white"
+    }
     }
   ));
 
 export default function Header(props){
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const handleChange = (e, value) => {
+    setValue(value);
+  }
     return(
       <React.Fragment>
         <ElevationScroll>
         <AppBar position="fixed">
             <Toolbar disableGutters>
                <img alt="company logo" className={classes.logo} src={logo} />
-               <Tabs className={classes.tabContainer}>
+               <Tabs value={value} onChange={handleChange} 
+                className={classes.tabContainer}>
                  <Tab className={classes.tab} label="Home" />
                  <Tab className={classes.tab} label="Services" />
                  <Tab className={classes.tab} label="The Revolution" />
                  <Tab className={classes.tab} label="About Us" />
                  <Tab className={classes.tab} label="Contact Us" />
                </Tabs>
+               <Button variant="contained" color="secondary" 
+               className={classes.buttonstyle} disableElevation>
+                Free Estimate
+              </Button>
             </Toolbar>
         </AppBar>
         </ElevationScroll>
