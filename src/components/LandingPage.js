da263dfe34503a2f7a54f0dcd16ab6777a6ae1a6
David <davidtucu@hotmail.com>
2021-11-18 13:17:57 -0300

    5. Creating The Hero Block styling 5.1 ButtonArrow

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import animationData from '../animations/landinganimation/data.js';
+import ButtonArrow from './ui/ButtonArrow.js';
 
 const useStyles = makeStyles(theme => ({
 
@@ -18,5 +21,32 @@ export default function LandingPage() {
         }
     };
 
-    return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
+    return (
+        <Grid container direction="column">
+            <Grid item>
+                <Grid container direcction="row">
+                    <Grid item>
+                        <div>Bringing west Coast Technology<br /> to the Midewest</div>
+                        <Grid container>
+                            <Grid item>
+                                <Button variant="contained">
+                                    Free Estimate
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined">
+                                    Learn More
+                                    <ButtonArrow width={15} height={15}
+                                    fill="red" />
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Lottie options={defaultOptions} height={"100%"} width={"100%"} /> 
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+        );
 }
